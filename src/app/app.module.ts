29179bc035d414b268ec17ef1b674e8d07f8c504
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-10-24 22:15:06 -0300

    criando componente de produtos e inserindo tabela para exibição de produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { ProdutosComponent } from './produtos/produtos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
